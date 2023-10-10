188c7899fe1d75b5b6d335789fb542b5eb387b1a
Chris Kildunne <136514462+kilzridesbikes@users.noreply.github.com>
2023-10-10 15:10:20 -0600

    images added to my work

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ export default function HomePage(){
     return(
         <>
         <h1>Your Hair Suks!</h1>
-        <p>Your hair suks is a ground-up movement started by me, Vinny, a guy whose hair really sucks. As long as you fools pay me too much money for mullets, I will keep doing it.</p>
+        <p>Im vinny. Your hair suks!</p>
         <img src="" alt="vinny" /> 
         </>
     )
--- src/pages/MyWork/MyWork.jsx
+++ src/pages/MyWork/MyWork.jsx
@@ -23,6 +23,22 @@ export default function MyWork() {
                 <div className="col-md-4">
                     <img src="https://i.imgur.com/bNDGlKb.jpg" alt="..." className="img-fluid" />
                 </div>
+              
+                <div className="col-md-4">
+                    <img src="https://i.imgur.com/S8P8XFL.jpg" alt="..." className="img-fluid" />
+                </div>
+                <div className="col-md-4">
+                    <img src="https://i.imgur.com/OzbFXbQ.jpg" alt="..." className="img-fluid" />
+                </div>
+                <div className="col-md-4">
+                    <img src="https://i.imgur.com/IyAmmLM.jpg" alt="..." className="img-fluid" />
+                </div>
+                <div className="col-md-4">
+                    <img src="https://i.imgur.com/fxgCzGz.png" alt="..." className="img-fluid" />
+                </div>
+                <div className="col-md-4">
+                    <img src="https://i.imgur.com/5wc6tmc.png" alt="..." className="img-fluid" />
+                </div>
             </div>
         </div>
     )
