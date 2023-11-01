46c3c8e3e5ceeb7a630507dbea340fe3d9bb425d
Chris Kildunne <136514462+kilzridesbikes@users.noreply.github.com>
2023-11-01 13:44:52 -0600

    navbar debugged for small screens

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,46 +6,46 @@ import './NavBar.css';
 
 export default function NavBar() {
   const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleToggleClick = () => {
+    setNavbarOpen(prevOpen => !prevOpen);
+  }
+
   const handleLinkClick = () => {
-    setNavbarOpen(false)
+    setNavbarOpen(false);
   }
 
   return (
-      <nav className="navbar navbar-expand-lg">
-        <button className="navbar-toggler" type="button" onClick={() => setNavbarOpen(!navbarOpen)}>
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className={`collapse navbar-collapse ${navbarOpen ? 'show' : ''}`} id="navbarNav">
+    <nav className="navbar navbar-expand-lg">
+      <button className="navbar-toggler" type="button" onClick={handleToggleClick} aria-controls="navbarNav" aria-expanded={navbarOpen}>
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className={`collapse navbar-collapse ${navbarOpen ? 'show' : ''}`} id="navbarNav">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item custom">
-            <Link className="nav-link" to="/" onClick = {handleLinkClick}><img src="https://i.imgur.com/lLFMaHX.png" alt="ransom-note-fonts" border="0"  style={{ width: '350px', height: '80px' , margin: '0px'}}></img></Link>
-          </li>
-          {/* <li className="nav-item custom">
-            <Link className="nav-link" to="/mywork"><img src="https://i.imgur.com/tKFzVAe.png" alt="ransom-note-fonts" border="0"  style={{ width: '400px', height: '80px' }}></img>
-     </Link>
-          </li> */}
-          <li className="nav-item cutsom">
-            <Link className="nav-link custom" to="/about"  onClick = {handleLinkClick}><img
-        src="https://i.imgur.com/cMuE2gu.png"
-        alt="ransom-note-fonts"
-        border="0"
-        style={{ width: '320px', height: '70px' }}
-      /></Link>
+            <Link className="nav-link" to="/" onClick={handleLinkClick}>
+              <img className="nav-image" src="https://i.imgur.com/lLFMaHX.png" alt="Home" style={{ width: '350px', height: '80px', margin: '0px' }} />
+              <span className="nav-text">Home</span>
+            </Link>
           </li>
           <li className="nav-item custom">
-            <Link className="nav-link" to="/contact"  onClick = {handleLinkClick}><img
-        src="https://i.imgur.com/ZTZGQbE.png"
-        alt="ransom-note-fonts"
-        border="0"
-        style={{ width: '380px', height: '70px' }}
-      /></Link>
+            <Link className="nav-link custom" to="/about" onClick={handleLinkClick}>
+              <img className="nav-image" src="https://i.imgur.com/cMuE2gu.png" alt="About" style={{ width: '320px', height: '70px' }} />
+              <span className="nav-text">About</span>
+            </Link>
+          </li>
+          <li className="nav-item custom">
+            <Link className="nav-link" to="/contact" onClick={handleLinkClick}>
+              <img className="nav-image" src="https://i.imgur.com/ZTZGQbE.png" alt="Contact" style={{ width: '380px', height: '70px' }} />
+              <span className="nav-text">Contact</span>
+            </Link>
           </li>
         </ul>
         <ul className="navbar-nav">
           <li className="nav-item custom">
-          <a href="https://www.instagram.com/yourhairsuks/" target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faInstagram} size="4x" className="instagram-icon" />
-                        </a>
+            <a href="https://www.instagram.com/yourhairsuks/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faInstagram} size="4x" className="instagram-icon" />
+            </a>
           </li>
         </ul>
       </div>
